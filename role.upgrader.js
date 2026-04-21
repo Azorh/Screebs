3582bc25799c5ef83705d5d5897c655de68ca415
Azorh <js.hetic@gmail.com>
2026-04-21 09:57:59 -0400

    Upgrader Logic Improvement & Autocomplete for VS and Webstorm
    Upgrader logic was remade to be the same as the repairer and builder. Using memory to store its current task.

--- role.upgrader.js
+++ role.upgrader.js
@@ -9,22 +9,33 @@ var roleUpgrader = {
         }
         if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
             creep.memory.upgrading = true;
-            creep.say('⚡ upgrade');
+            creep.say('🚧 Upgrade');
         }
 
         if(creep.memory.upgrading) {
-            if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller, {
-                    visualizePathStyle: {stroke: '#ffffff'},
-                    ignoreCreeps: true
-                });
+            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+
+            if(targets.length) {
+                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {
+                        visualizePathStyle: {stroke: '#ffffff'},
+                        ignoreCreeps: true
+                    });
+                }
+            } else {
+                if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(creep.room.controller, {
+                        visualizePathStyle: {stroke: '#ffffff'},
+                        ignoreCreeps: true
+                    });
+                }
             }
         }
         else {
             var source = creep.pos.findClosestByPath(FIND_SOURCES);
             if (source && creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(source, {
-                    visualizePathStyle: {stroke: '#ebe709'},
+                    visualizePathStyle: {stroke: '#ffaa00'},
                     ignoreCreeps: true
                 });
             }
